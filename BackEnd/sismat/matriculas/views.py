5d21f69505302a44fd2be2550430aa012bf98004
RyanValdivia <rvaldiviase@unsa.edu.pe>
2024-06-21 10:00:26 -0500

    Completando serializadores

--- BackEnd/sismat/matriculas/views.py
+++ BackEnd/sismat/matriculas/views.py
@@ -19,49 +19,44 @@ class CourseListCreate(generics.ListCreateAPIView):
 class CourseDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+    
+class LaboratoryListCreate(generics.ListCreateAPIView):
+    queryset = Laboratory.objects.all()
+    serializer_class = LaboratorySerializer
 
 class LaboratoryDetail(generics.RetrieveUpdateAPIView):
     queryset = Laboratory.objects.all()
     serializer_class = LaboratorySerializer
+    
+class InscriptionListCreate(generics.ListCreateAPIView):
+    queryset = Inscription.objects.all()
+    serializer_class = InscriptionSerializer
         
 class InscriptionDetail(generics.RetrieveUpdateAPIView):
     queryset = Inscription.objects.all()
     serializer_class = InscriptionSerializer
-
+    
+class InscriptionLabListCreate(generics.ListCreateAPIView):
+    queryset = InscriptionLab.objects.all()
+    serializer_class = InscriptionSerializer
 class InscriptionLabDetail(generics.RetrieveUpdateAPIView):
     queryset = InscriptionLab.objects.all()
     serializer_class = InscriptionLabSerializer
 
+class StudentListCreate(generics.ListCreateAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    
 class StudentDetail(generics.RetrieveUpdateAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
-class TeacherDetail(generics.RetrieveUpdateAPIView):
-    queryset = Teacher.objects.all()
-    serializer_class = TeacherSerializer
-
-class WorkloadDetail(generics.RetrieveUpdateAPIView):
-    queryset = Workload.objects.all()
-    serializer_class = WorkloadSerializer
-
-class WorkloadLabDetail(generics.RetrieveUpdateAPIView):
-    queryset = WorkloadLab.objects.all()
-    serializer_class = WorkloadLabSerializer
-
-
-class InscriptionListCreate(generics.ListCreateAPIView):
-    queryset = Inscription.objects.all()
-    serializer_class = InscriptionSerializer
-
-class InscriptionLabListCreate(generics.ListCreateAPIView):
-    queryset = InscriptionLab.objects.all()
-    serializer_class = InscriptionSerializer
-
-class StudentListCreate(generics.ListCreateAPIView):
-    queryset = Student.objects.all()
-    serializer_class = StudentSerializer
 
 class TeacherListCreate(generics.ListCreateAPIView):
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
+    
+class TeacherDetail(generics.RetrieveUpdateAPIView):
     queryset = Teacher.objects.all()
     serializer_class = TeacherSerializer
 
@@ -69,7 +64,15 @@ class WorkloadListCreate(generics.ListCreateAPIView):
     queryset = Workload.objects.all()
     serializer_class = WorkloadSerializer
 
+class WorkloadDetail(generics.RetrieveUpdateAPIView):
+    queryset = Workload.objects.all()
+    serializer_class = WorkloadSerializer
+
 class WorkloadLabListCreate(generics.ListCreateAPIView):
+    queryset = WorkloadLab.objects.all()
+    serializer_class = WorkloadLabSerializer
+    
+class WorkloadLabDetail(generics.RetrieveUpdateAPIView):
     queryset = WorkloadLab.objects.all()
     serializer_class = WorkloadLabSerializer
 
