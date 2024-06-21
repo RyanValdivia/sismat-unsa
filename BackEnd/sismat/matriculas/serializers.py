9be4891d99e711ad7f5e5a8c0e2fea99aa70bedf
Johann-1498 <jcaceresruiz@unsa.edu.pe>
2024-06-21 01:18:24 -0500

    Implementacion de serializadores y vistas para cada modelo

--- BackEnd/sismat/matriculas/serializers.py
+++ BackEnd/sismat/matriculas/serializers.py
@@ -1,10 +1,50 @@
 from rest_framework import serializers
 
 from .models.Course import Course, Laboratory
+from .models.Inscription import Inscription, InscriptionLab
+from .models.Student import Student
+from .models.Teacher import Teacher
+from .models.Workload import Workload, WorkloadLab
 
-        
+
 class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
         fields = ['id', 'code', 'name', 'hasLaboratory', 'status', 'year', 'semester']
+
+class LaboratorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Laboratory
+        fields = ['id', 'course_id', 'name', 'status', 'year', 'semester']
+
+class InscriptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Inscription
+        fields = ['id', 'student_id', 'workload_id','status', 'course_status', 'nro_matricula']
+
+class InscriptionLabSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = InscriptionLab
+        fields = ['id', 'student_id', 'workload_id','status', 'course_status', 'nro_matricula' ]
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = ['id', 'cui', 'names', 'lastnames', 'email', 'status']
+
+class TeacherSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Teacher
+        fields = ['id', 'email', 'names', 'lastnames', 'status']
+    
+class WorkloadSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Workload
+        fields = ['id', 'group', 'capacity', 'year', 'semester', 'teacher_id', 'course_id']
+
+class WorkloadLabSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = WorkloadLab
+        fields = ['id', 'group', 'capacity', 'year', 'semester', 'teacher_id', 'laboratory_id']
         
+
--- BackEnd/sismat/matriculas/views.py
+++ BackEnd/sismat/matriculas/views.py
@@ -3,8 +3,12 @@ from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework import status
-from matriculas.models.Course import Course
-from .serializers import CourseSerializer
+from matriculas.models.Course import Course, Laboratory
+from matriculas.models.Inscription import Inscription, InscriptionLab
+from matriculas.models.Student import Student
+from matriculas.models.Teacher import Teacher
+from matriculas.models.Workload import Workload, WorkloadLab
+from .serializers import CourseSerializer, LaboratorySerializer, InscriptionSerializer, InscriptionLabSerializer, StudentSerializer, TeacherSerializer, WorkloadSerializer, WorkloadLabSerializer
 
 # Create your views here.
 
@@ -15,4 +19,58 @@ class CourseListCreate(generics.ListCreateAPIView):
 class CourseDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+
+class LaboratoryDetail(generics.RetrieveUpdateAPIView):
+    queryset = Laboratory.objects.all()
+    serializer_class = LaboratorySerializer
         
+class InscriptionDetail(generics.RetrieveUpdateAPIView):
+    queryset = Inscription.objects.all()
+    serializer_class = InscriptionSerializer
+
+class InscriptionLabDetail(generics.RetrieveUpdateAPIView):
+    queryset = InscriptionLab.objects.all()
+    serializer_class = InscriptionLabSerializer
+
+class StudentDetail(generics.RetrieveUpdateAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+class TeacherDetail(generics.RetrieveUpdateAPIView):
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
+
+class WorkloadDetail(generics.RetrieveUpdateAPIView):
+    queryset = Workload.objects.all()
+    serializer_class = WorkloadSerializer
+
+class WorkloadLabDetail(generics.RetrieveUpdateAPIView):
+    queryset = WorkloadLab.objects.all()
+    serializer_class = WorkloadLabSerializer
+
+
+class InscriptionListCreate(generics.ListCreateAPIView):
+    queryset = Inscription.objects.all()
+    serializer_class = InscriptionSerializer
+
+class InscriptionLabListCreate(generics.ListCreateAPIView):
+    queryset = InscriptionLab.objects.all()
+    serializer_class = InscriptionSerializer
+
+class StudentListCreate(generics.ListCreateAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+class TeacherListCreate(generics.ListCreateAPIView):
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
+
+class WorkloadListCreate(generics.ListCreateAPIView):
+    queryset = Workload.objects.all()
+    serializer_class = WorkloadSerializer
+
+class WorkloadLabListCreate(generics.ListCreateAPIView):
+    queryset = WorkloadLab.objects.all()
+    serializer_class = WorkloadLabSerializer
+
+
