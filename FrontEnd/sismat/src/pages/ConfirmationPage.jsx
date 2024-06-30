fbe05d8314f5422b947693a71af4ed9b6855b800
JBenjamin01 <jmamanices@unsa.edu.pe>
2024-06-30 08:26:10 -0500

    Cambiando el color del fondo del diseño de la página

--- FrontEnd/sismat/src/pages/ConfirmationPage.jsx
+++ FrontEnd/sismat/src/pages/ConfirmationPage.jsx
@@ -1,15 +1,15 @@
 import ConfirmationTable from "../components/ConfirmationTable.jsx";
 
-const ConfirmationPage = () =>{
+const ConfirmationPage = () => {
     return (
-        <div className="flex flex-col min-h-screen w-full bg-gradient-to-tr from-[#8B0000] to-[#800020] pb-5">
-            <header className="flex items-center justify-between bg-[#9B0000] text-white py-4 px-6">
-                <div className="font-bold">Nombre del usuario :v</div>
-                <div className=""><a href="#">Sign Out</a></div>
+        <div className="flex flex-col min-h-screen w-full bg-gray-200 pb-5">
+            <header className="flex items-center justify-between bg-[#8B0000] text-white py-4 px-6">
+                <div className="font-bold text-xl">Nombre del usuario :v</div>
+                <div><a href="#" className="text-white hover:text-gray-300">Sign Out</a></div>
             </header>
             
             <div className="flex flex-col flex-1 items-center w-full">
-                <div className="w-full max-w-7xl">
+                <div className="w-full max-w-7xl mt-10">
                     <ConfirmationTable />
                 </div>
             </div>
