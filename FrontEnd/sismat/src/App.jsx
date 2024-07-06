cd3306034adf51910ddde0b6d836f1dda657c8fb
Johann-1498 <jcaceresruiz@unsa.edu.pe>
2024-07-06 18:10:29 -0500

    Modificacion en pagina de selección para la interaccion al elegir cursos y se sume las credenciales seleccionadas

--- FrontEnd/sismat/src/App.jsx
+++ FrontEnd/sismat/src/App.jsx
@@ -3,6 +3,7 @@ import LoginPage from "./pages/LoginPage";
 import SelectCoursePage from "./pages/SelectCoursePage";
 import SelectGroupPage from "./pages/SelectGroupPage";
 import ConfirmationPage from "./pages/ConfirmationPage";
+import RegisterPage from "./pages/RegisterPage";
 
 const App = () => {
     return (
