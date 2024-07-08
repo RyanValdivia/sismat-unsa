0efda255ce6553a75f0232c80eb2cd78f722fab5
RyanValdivia <rvaldiviase@unsa.edu.pe>
2024-07-07 22:44:26 -0500

    Preparando para deploy

--- BackEnd/sismat/build.sh
+++ BackEnd/sismat/build.sh
@@ -11,4 +11,4 @@ python manage.py collectstatic --no-input
 # Apply any outstanding database migrations
 python manage.py migrate
 
-python create_superuser.py
+python manage.py createsuperuser --no-input --username admin --password 1234
