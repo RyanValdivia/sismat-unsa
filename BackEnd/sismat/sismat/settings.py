6c9151a40a1d07693953eb52f1c87e864e73e170
RyanValdivia <rvaldiviase@unsa.edu.pe>
2024-07-17 10:50:44 -0500

    Revert "Preparando para deploy"
    This reverts commit 67ebad52a63486fd0717ecd992d59a73e982dc25.

--- BackEnd/sismat/sismat/settings.py
+++ BackEnd/sismat/sismat/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
