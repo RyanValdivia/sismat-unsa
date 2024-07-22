2bd1436a98816060a8862bb0c378d798b8901ffd
Jhonatan Benjamin Mamani Céspedes <jmamanices@unsa.edu.pe>
2024-07-22 15:53:45 -0500

    Obteniendo la data de los Workloads con los atributos de su profesor y curso, probando información guardada en sessionstorage

---
+++ FrontEnd/sismat/src/api/works.js
@@ -0,0 +1,52 @@
+import axios from "axios";
+
+export async function getWorkload(token) {
+    const api = axios.create({
+        baseURL: "http://127.0.0.1:8000/api/",
+        timeout: 5000,
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + token
+        }
+    });
+    const response = await api.get("/workloads/");
+    return response.data;
+}
+
+export async function getTeacherDetails(token, teacherId) {
+    const api = axios.create({
+        baseURL: "http://127.0.0.1:8000/api/",
+        timeout: 5000,
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + token
+        }
+    });
+    const response = await api.get("/teachers/"+ teacherId);
+    return response.data;
+}
+
+export async function getCourseDetails(token, courseId) {
+    const api = axios.create({
+        baseURL: "http://127.0.0.1:8000/api/",
+        timeout: 5000,
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + token
+        }
+    });
+    const response = await api.get("/courses/"+ courseId);
+    return response.data;
+}
+
+export async function refreshToken(token) {
+    const api = axios.create({
+        baseURL: "http://127.0.0.1:8000/api/",
+        timeout: 5000,
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + token
+        },
+    });
+    return api.post("/token/refresh/", { refresh: token });
+}
